d03b29e481e16fcb93d728347fd5e601c3a36f13
hamidreza82m <hamidreza82m@gmail.com>
2025-01-29 00:48:50 +0330

    ok

--- index8.js
+++ index8.js
@@ -1,50 +1,48 @@
-document.getElementById('login').addEventListener('click', function () {
-    // const elements = document.querySelectorAll('#black');
-    // elements.forEach(element => {
-    // element.style.transform='rotate(-45deg)';
-    // element.style.left='-450px'
-    document.getElementById('black').classList.add('animate');
-    document.getElementById('black').classList.remove('animate2');
 
-    // });
-});
 document.getElementById('login').addEventListener('click', function () {
     const elements = document.querySelectorAll('#signUp');
     elements.forEach(element => {
         element.style.display = 'none';
     });
-})
-document.getElementById('login').addEventListener('click', function () {
-    const elements = document.querySelectorAll('#login-page');
-    elements.forEach(element => {
+    document.getElementById('black').classList.add('animate');
+    document.getElementById('black').classList.remove('animate2');
+
+    const elements2 = document.querySelectorAll('#login-page');
+    elements2.forEach(element => {
         element.style.display = 'flex';
     });
+    document.querySelectorAll('.rotate2').forEach(function (element) {
+        element.classList.add('animate3');
+    });
+    const elements3 = document.querySelectorAll('.text-black-p');
+    elements3.forEach(element => {
+        element.style.position = 'relative';
+    })
+    document.querySelectorAll('.translate').forEach(function (element) {
+        element.classList.add('animate4');
+    })
+    document.querySelectorAll('.translate2').forEach(function (element) {
+        element.classList.add('animate5');
+    })
 })
+
 document.getElementById('signUpButton').addEventListener('click', function () {
     document.getElementById('black').classList.add('animate2');
     document.getElementById('black').classList.remove('animate');
 
-    // });
-});
-document.getElementById('signUpButton').addEventListener('click', function () {
     const elements = document.querySelectorAll('#signUp');
     elements.forEach(element => {
         element.style.display = 'flex';
     });
-})
-document.getElementById('signUpButton').addEventListener('click', function () {
-    const elements = document.querySelectorAll('#login-page');
-    elements.forEach(element => {
+    const elements2 = document.querySelectorAll('#login-page');
+    elements2.forEach(element => {
         element.style.display = 'none';
     });
-})
-document.getElementById('signUpButton').addEventListener('click', function () {
-document.querySelectorAll('.rotate').forEach(function(element) {
-     element.classList.add('animate3');
+    document.querySelectorAll('.rotate').forEach(function (element) {
+        element.classList.add('animate3');
     });
+    const elements3 = document.querySelectorAll('.text-black-p');
+    elements3.forEach(element => {
+        element.style.position = 'absolute';
+    })
 });
-document.getElementById('login').addEventListener('click', function () {
-    document.querySelectorAll('.rotate2').forEach(function(element) {
-         element.classList.add('animate3');
-        });
-    });
