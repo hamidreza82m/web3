4c90749d102efe54ff9b9b5570c21137b038a89c
hamidreza82m <hamidreza82m@gmail.com>
2025-02-11 18:59:12 +0330

    ماشین حساب

---
+++ index12.js
@@ -0,0 +1,29 @@
+let firstnum = "";
+let op = "";
+let Result;
+let secondnum="";
+
+function number(num) {
+    document.getElementById('input').value += num;
+}
+function opration(opration) {
+    firstnum = document.getElementById('input').value;
+    op = opration;
+    document.getElementById('input-mini').value = document.getElementById('input').value + op;
+    document.getElementById('input').value = "";
+}
+function result() {
+    secondnum = document.getElementById('input').value;
+    switch (op) {
+        case '+':Result = parseFloat(firstnum) + parseFloat(secondnum);break;
+        case '-':Result = parseFloat(firstnum) - parseFloat(secondnum);break;
+        case '*':Result = parseFloat(firstnum) * parseFloat(secondnum);break;
+        case '/':Result = parseFloat(firstnum) / parseFloat(secondnum);break;
+    }
+    document.getElementById('input-mini').value += secondnum + "=";
+    document.getElementById('input').value = Result
+}
+
+function clearInput() {
+    document.getElementById('input').value = "";
+}
